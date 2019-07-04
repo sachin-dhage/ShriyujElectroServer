ea47923db95cacd53358543ef4415eae3a9e143e
sachin-dhage <43755446+sachin-dhage@users.noreply.github.com>
2019-07-04 18:50:22 +0530

    Update index.js

--- config/util/index.js
+++ config/util/index.js
@@ -1,5 +1,4 @@
-const APP_SECRET = `Al·go·rith·mus~algorithm!algoritme@ስልተ#خوارزمية$ալգորիթմ%alqoritm^অ্যালগরিদম&алгарытм*算法(算法)ალგორითმი_αλγόριθμος+અલ્ગોરિધમનો=אלגוריתם-एल्गोरिथ्म0reiknirit9アルゴリズム8ಕ್ರಮಾವಳಿ7
-ក្បួនដោះស្រាយ6연산5Cureyê4ຂັ້ນຕອນວິທີ3അൽഗോരിതം2hātepe1अल्गोरिदम`;
+const APP_SECRET = ``;
 
 
 module.exports = {
